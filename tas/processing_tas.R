a0bf7c76f471e8b33e5a0870e0732317e0dec516
leeyap <leeya.pressburger@pnnl.gov>
2022-01-13 15:48:40 -0500

    Updates 1-13
    getting organized to import more data

--- tas/processing_tas.R
+++ tas/processing_tas.R
@@ -2,21 +2,30 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 library(ncdf4)
+library(here)
 
-mypath = "C:/Users/pres520/PycharmProjects/pangeo/test"
+BASE_DIR <- here::here()
 
-files <- list.files(path = mypath, pattern = "*.csv", full.names = TRUE)
+mypath <-  "C:/Users/pres520/PycharmProjects/pangeo/test/tas"
+path1 <- file.path(BASE_DIR, "csv1")
+path2 <- file.path(BASE_DIR, "csv2")
 
-# Remove non-model run - "/pangeo_table.csv"
-nums <- c(1:956, 958:1045)
-files <- files[nums]
+files1 <- list.files(path = path1, pattern = "*.csv", full.names = TRUE)
+files2 <- list.files(path = path2, pattern = "*.csv", full.names = TRUE)
 
 # Read in data, forcing column types
-data <- lapply(files, read_csv, col_types = "dccccccdd") %>%
+data1 <- lapply(files1, read_csv, col_types = "dccccccdd") %>%
   bind_rows(.id = "File")
 
+nums <- c(1:474, 476:563)
+
+data2 <- lapply(files2[nums], read_csv, col_types = "dccccccdd") %>% 
+  bind_rows(.id = "File")
+
+data <- bind_rows(data1, data2)
+
 # Add name identifier
-data$name <- paste0(data$model, "_", data$ensemble, "_", data$variable)
+data$name <- paste0(data$model, "_", data$ensemble, "_", data$experiment)
 
 ## Get historical average
 testing <- data %>%
@@ -39,10 +48,45 @@ Tgav <- data %>%
 weird_range <- data %>% filter(year < 1850 | year > 2500)
 weird_range <- weird_range %>% mutate(ncdf = paste0(model, "_", ensemble, "_", experiment))
 weird_models <- unique(weird_range$name)
+
 model_ncdf <- as.data.frame(unique(weird_range$ncdf))
 colnames(model_ncdf) <- "model"
+model_ncdf <- model_ncdf %>% mutate(filename = paste0(mypath, "/", model, ".nc"))
 
-# Normal years (1850-2100)
+# We want to open net cdf file, extract time information, and loop to normalize
+results <- lapply(model_ncdf$filename, nc_open)
+results <- lapply(results, format_time) %>% bind_rows()
+
+
+
+## Fix dates
+# Extract and format output time results. 
+# Arguments 
+#   nc: the nc data of the file with the time information to extract. 
+# Returns: A data frame of the time formated as time date information. 
+format_time <- function(nc){
+  
+  # Make sure that the object being read in is a netcdf file.
+  assertthat::assert_that(class(nc) == "ncdf4")
+  
+  # Convert from relative time to absoulte time using lubridate.
+  time_units <- ncdf4::ncatt_get(nc, 'time')$units
+  time_units <- gsub(pattern = 'days since ', replacement = '', time_units)
+  time <- lubridate::as_date(ncdf4::ncvar_get(nc, 'time'), origin = time_units)
+  
+  data.frame(datetime = time,
+             year = lubridate::year(time),
+             month = lubridate::month(time),
+             stringsAsFactors = FALSE)
+  
+}
+
+
+## Plot results
+
+results <- lapply(model_ncdf$filename, format_time)
+
+# Normal years (1850-2500)
 normal_range <- data %>% filter(year > 1849 & year < 2500)
 
 plot_normal <- normal_range %>% 
@@ -86,5 +130,4 @@ plot_ssps <- ssps %>%
        title = "CMIP6 runs - tas over time") +
   facet_wrap(~model) +
   theme_minimal()
-
 
